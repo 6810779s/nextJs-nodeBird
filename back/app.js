58c0e2c235553a25d99faa5e09bea55ab55a1b87
Eunhee <6810779s@github.com>
2022-01-20 17:52:20 +0900

    git ignore settings

--- back/app.js
+++ back/app.js
@@ -1,36 +1,25 @@
 const express = require("express");
 const app = express();
-const postRouter = require("./routes/posts");
-/*
-자주쓰이는 용어
-app.get 가져오다
-app.post 생성하다
-app.put 전체수정
-app.delete 제거
-app.patch 부분 수정
-app.options 찔러보기, 요청 보낼 수 있는지 없는지 서버에 찔러보기
-app.head 헤더만 가져오기
+const { executeStatement, connection } = require("./server");
 
-애매하면 그냥 post사용하면 됨. 팀원끼리 타협해서 사용하면 됨.
-*/
+app.listen(8080, function () {
+  console.log("listening on 8080");
+});
+const information = [];
 app.get("/", (req, res) => {
-  res.send("hello express");
+  const content = "select * from [guidancepro].[dbo].[user_testDB]";
+  connection.on("connect", function (err) {
+    if (err) {
+      console.log("err:", err);
+    } else {
+      console.log("Connected");
+      res.send("express");
+      executeStatement(content);
+    }
+  });
+  connection.connect();
 });
 
-app.get("/", (req, res) => {
-  res.send("hello api");
-});
-app.get("/posts", (req, res) => {
-  res.json([
-    { id: 1, content: "hello" },
-    { id: 2, content: "hello2" },
-    { id: 3, content: "hello3" },
-    { id: 4, content: "hello4" },
-  ]);
-});
-
-app.use('/post',postRouter); //
-
-app.listen(3002, () => {
-  console.log("3002포트에서 서버 실행");
+app.get("/welcome", (req, res) => {
+  res.send(information);
 });
