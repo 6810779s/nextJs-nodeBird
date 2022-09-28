422eafd346d14d563fe9a1dadc6615ae449a6f18
eunhee <6810779s@naver.com>
2022-09-28 19:25:47 +0900

    env seeting

--- back/config/config.js
+++ back/config/config.js
@@ -3,22 +3,22 @@ dotenv.config();
 
 module.exports = {
   development: {
-    username: "root",
-    password: process.env.DB_PASSWORD,
+    username: `${process.env.DB_USER}`,
+    password: `${process.env.DB_PASSWORD}`,
     database: "react_nodebird",
     host: "127.0.0.1",
     dialect: "mysql",
   },
   test: {
-    username: "root",
-    password: process.env.DB_PASSWORD,
+    username: `${process.env.DB_USER}`,
+    password: `${process.env.DB_PASSWORD}`,
     database: "react_nodebird",
     host: "127.0.0.1",
     dialect: "mysql",
   },
   production: {
-    username: "root",
-    password: process.env.DB_PASSWORD,
+    username: `${process.env.DB_USER}`,
+    password: `${process.env.DB_PASSWORD}`,
     database: "react_nodebird",
     host: "127.0.0.1",
     dialect: "mysql",
