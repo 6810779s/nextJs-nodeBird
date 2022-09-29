c64af9b7d4f45332f35317de10600e44ae547f38
eunhee <6810779s@naver.com>
2022-09-29 15:37:52 +0900

    assetPrefix

--- front/next.config.js
+++ front/next.config.js
@@ -4,12 +4,15 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 
 module.exports = withBundleAnalyzer({
   compress: true,
+  assetPrefix:
+    process.env.NODE_ENV === "production" ? "http://44.209.7.224" : "",
   webpack(config, { webpack }) {
     const prod = process.env.NODE_ENV === "production";
     return {
       ...config,
       mode: prod ? "production" : "development",
       devtool: prod ? "hidden-source-map" : "eval",
+
       plugins: [
         ...config.plugins,
         new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /^\.\/ko$/),
